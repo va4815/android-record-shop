aa77b01a8cc208c086cfa89b23bdd1db6e4c78d3
Vincent <va543567@gmail.com>
2024-12-16 11:22:26 +0000

    feat: added get all albums API

--- app/src/main/java/com/northcoders/recordshop/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshop/service/AlbumApiService.java
@@ -1,4 +1,15 @@
 package com.northcoders.recordshop.service;
 
+import com.northcoders.recordshop.model.Album;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface AlbumApiService {
+
+    @GET("/albums")
+    Call<List<Album>> getAllAlbums();
+
 }
