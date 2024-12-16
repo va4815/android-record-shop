a5abbc293fd8033492652f640fefa06addab26c6
Vincent <va543567@gmail.com>
2024-12-16 11:53:58 +0000

    feat: implement get all albums API in Main Activity

--- app/src/main/java/com/northcoders/recordshop/MainActivity.java
+++ app/src/main/java/com/northcoders/recordshop/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.northcoders.recordshop.model.AlbumRepository;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        AlbumRepository albumRepository = new AlbumRepository(this.getApplication());
+
+        albumRepository.getMutableLiveData();
+
     }
 }
