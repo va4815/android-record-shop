6525a51348fec0feeb91c9bac82f94c854e39213
Vincent <va543567@gmail.com>
2024-12-16 11:11:51 +0000

    feat: added retrofit instance and album api service

---
+++ app/src/main/java/com/northcoders/recordshop/service/RetrofitInstance.java
@@ -0,0 +1,30 @@
+package com.northcoders.recordshop.service;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "http://192.168.50.60:8080/api/v1";
+
+    public static AlbumApiService getService() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
+                    .build();
+        }
+
+        return retrofit.create(AlbumApiService.class);
+    }
+
+}
