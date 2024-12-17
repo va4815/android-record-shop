35d4c67cef96c0fc00dd5b48dc6a69002850e312
Vincent <va543567@gmail.com>
2024-12-17 11:53:08 +0000

    feat: implemented album adapter in main activity

--- app/src/main/java/com/northcoders/recordshop/MainActivity.java
+++ app/src/main/java/com/northcoders/recordshop/MainActivity.java
@@ -7,25 +7,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.northcoders.recordshop.databinding.ActivityMainBinding;
+import com.northcoders.recordshop.model.Album;
 import com.northcoders.recordshop.model.AlbumRepository;
+import com.northcoders.recordshop.ui.mainactivity.AlbumAdapter;
+import com.northcoders.recordshop.ui.mainactivity.MainActivityViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<Album> albumList;
+    private AlbumAdapter albumAdapter;
+    private ActivityMainBinding binding;
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        AlbumRepository albumRepository = new AlbumRepository(this.getApplication());
-
-        albumRepository.getMutableLiveData();
+        getAllAlbums();
 
     }
+
+    private void getAllAlbums() {
+        viewModel.getAllAlbums().observe(this, new Observer<List<Album>>() {
+            @Override
+            public void onChanged(List<Album> albums) {
+                albumList = albums;
+                displayAlbumsInRecyclerView();
+            }
+        });
+    }
+
+    private void displayAlbumsInRecyclerView() {
+        recyclerView = binding.mainRecyclerView;
+        albumAdapter = new AlbumAdapter(albumList);
+        recyclerView.setAdapter(albumAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        albumAdapter.notifyDataSetChanged();
+    }
+
 }
